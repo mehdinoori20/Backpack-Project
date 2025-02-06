9e845343371f8ccc66dfcda2dfe86ad8b137a9e3
mehdinoori20 <mehdi.noori1994@hotmail.com>
2025-02-06 12:58:53 +0100

    converted to react native

--- Backpack-Project/src/App.tsx
+++ Backpack-Project/src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import React from "react"
+import { Text, View, StyleSheet } from "react-native"
+
+const App = () => {
   return (
-    <div>
-      <h1>Welcome to the BackPack Project</h1>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.text}>Välkommen till React Native!</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  text: {
+    fontSize: 18,
+    color: "#333",
+  },
+})
 
 export default App
